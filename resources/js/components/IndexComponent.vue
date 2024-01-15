db7cc74a83f19ea0630aa18630b8d1cfe57e8cbe
Roman Skobenkov <r.skobenkov@intensa.ru>
2024-01-15 07:39:09 +0300

    vue - Урок 17 (toggler элементов и ещё про двоеточие в темплэйте)

--- resources/js/components/IndexComponent.vue
+++ resources/js/components/IndexComponent.vue
@@ -4,7 +4,9 @@ export default {
 
     data() {
         return {
-            people: null
+            people: null,
+            // свойство для хранения текущей редактироуемой персоны
+            editPersonId: null
         }
     },
 
@@ -19,6 +21,17 @@ export default {
                 .then( res => {
                     this.people = res.data;
                 });
+        },
+
+        // переключение редактируемой персоны
+        changePersonEditId(id) {
+            this.editPersonId = id;
+        },
+
+        // проверка, что текущий элемент - редактируемый (для отображения строки с полями редактирования)
+        // в первый раз всегда вернёт false, потому что изначально в data editPersonId равен null
+        isEdit(id) {
+            return this.editPersonId === id;
         }
     }
 }
@@ -33,15 +46,27 @@ export default {
                 <th scope="col">Name</th>
                 <th scope="col">Age</th>
                 <th scope="col">Job</th>
+                <th scope="col">Edit</th>
             </tr>
             </thead>
             <tbody>
-            <tr v-for="person in people">
-                <th scope="row">{{ person.id }}</th>
-                <td>{{ person.name }}</td>
-                <td>{{ person.age }}</td>
-                <td>{{ person.job }}</td>
-            </tr>
+            <template v-for="person in people">
+                <tr>
+                    <th scope="row">{{ person.id }}</th>
+                    <td>{{ person.name }}</td>
+                    <td>{{ person.age }}</td>
+                    <td>{{ person.job }}</td>
+                    <td><a href="#" @click.prevent="changePersonEditId(person.id)" class="btn btn-success">Edit</a></td>
+                </tr>
+                <tr :class="isEdit(person.id) ? '' : 'd-none'">
+                    <th scope="row">{{ person.id }}</th>
+                    <td><input type="text" class="form-control"></td>
+                    <td><input type="number" class="form-control"></td>
+                    <td><input type="text" class="form-control"></td>
+                    <!-- передаём null для скрывания текущей строки редактирования -->
+                    <td><a href="#" @click.prevent="changePersonEditId(null)" class="btn btn-success">Update</a></td>
+                </tr>
+            </template>
             </tbody>
         </table>
     </div>
