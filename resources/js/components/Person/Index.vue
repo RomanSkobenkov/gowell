9fcf621565e570ad65f2adfbc2a65a872823a39f
Roman Skobenkov <r.skobenkov@intensa.ru>
2024-01-22 02:57:20 +0300

    vue - Урок 30 (удаление и ссылки на детальные страницы)

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -21,6 +21,13 @@ export default {
                 .then(res => {
                     this.people = res.data;
                 });
+        },
+
+        deletePerson(id) {
+            axios.delete(`/api/people/${id}`)
+                .then(res => {
+                    this.getPeople();
+                });
         }
     },
 }
@@ -34,15 +41,21 @@ export default {
             <th scope="col">Age</th>
             <th scope="col">Job</th>
             <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
         </tr>
         </thead>
         <tbody>
         <tr v-for="person in people">
-            <td>{{ person.name }}</td>
+            <td>
+                <router-link :to="{ name: 'person.show', params: {id: person.id} }">{{ person.name }}</router-link>
+            </td>
             <td>{{ person.age }}</td>
             <td>{{ person.job }}</td>
             <td>
                 <router-link :to="{ name: 'person.edit', params: {id: person.id} }">Edit</router-link>
+            </td>
+            <td>
+                <a href="#" @click.prevent="deletePerson(person.id)" class="btn btn-outline-danger">Delete</a>
             </td>
         </tr>
         </tbody>
